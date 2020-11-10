0d8d6749ce30b5586f40407a570985626753460a
Alden Davidson <adavidson+github@protonmail.ch>
2020-11-10 12:47:59 -0800

    added cookieParser and welcome user on hello page

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-// app.use(cookieParser('keepitsecretkeepitsafe'));
+app.use(cookieParser());
 
 app.post('/login', (req, res) => {
   let name = req.body.name;
   if (name) {
-    res.cookie("name", name)
+    res.cookie("name", name);
     //   signed: true,
     //   secure: true,
     //   httpOnly: true
@@ -24,6 +24,12 @@ app.post('/login', (req, res) => {
     res.json(result);
   }
 });
+
+app.get('/hello', (req, res) => {
+  if (req.cookies.name) {
+    res.send(`Welcome ${req.cookies.name}!`)
+  }
+})
 
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
